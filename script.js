1784a63d5f2c06eb75a14eeaf63b6e59aadcad06
WH1T3 DR4GON <162749588+whitedragon3257@users.noreply.github.com>
2024-03-19 14:36:21 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Funcoes relacionadas ao menu Header
+function goToHome(){window.open('./index.html');}           // Abertura da pagina principal
+function openContact(){window.open('wa.me/5562993882350');} // Abertura da API do WhatsApp
