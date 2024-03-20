5aa981be4e0f3b59ed8c989d56ba7757cdb7ea7e
WH1T3 DR4GON <162749588+whitedragon3257@users.noreply.github.com>
2024-03-20 17:53:19 -0300

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,3 +4,4 @@ function goToArduinoP1(){open('https://github.com/whitedragon3257/Arduino_P1');}
 function openContact(){window.open('https://api.whatsapp.com/send/?phone=562993882350&text&type=phone_number&app_absent=0');}
 function openGMAIL(){window.open('mailto:whitedragon36915@gmail.com');}
 function openGITHUB(){window.open('https://github.com/whitedragon3257');}
+function goToWorms()(window.open('https://github.com/whitedragon3257/The_Magic');}
